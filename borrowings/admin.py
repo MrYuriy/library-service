746cdc2626d97570d80f71191d7e2062e2d45886
YuriiBabiuk <118677914+YuriiBabiuk@users.noreply.github.com>
2023-07-16 15:58:55 +0300

    Fix errors with admin pannel

--- borrowings/admin.py
+++ borrowings/admin.py
@@ -3,6 +3,6 @@ from borrowings.models import Borrowing
 
 @admin.register(Borrowing)
 class BorrowingAdmin(admin.ModelAdmin):
-    list_display = ('book', 'user', 'borrow_date', 'expected_return_date', 'actual_return_date')
-    list_filter = ('borrow_date', 'expected_return_date')
+    list_display = ('book', 'user', 'borrow_date', 'extend_return_date', 'actual_return_date')
+    list_filter = ('borrow_date', 'actual_return_date')
     search_fields = ('book__title', 'user__email')
--- borrowings/models.py
+++ borrowings/models.py
@@ -4,8 +4,8 @@ from library_service import settings
 
 class Borrowing(models.Model):
     borrow_date = models.DateField()
-    extend_return = models.DateField()
-    actual_return = models.DateField(null=True, blank=True)
+    extend_return_date = models.DateField()
+    actual_return_date = models.DateField(null=True, blank=True)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
