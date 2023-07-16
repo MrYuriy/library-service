032212ec09fcf202807c80d1680f35dedec3f0b9
YuriiBabiuk <118677914+YuriiBabiuk@users.noreply.github.com>
2023-07-16 15:03:20 +0300

    Add empty urls.py

--- library_service/urls.py
+++ library_service/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("user/", include("user.urls", namespace="user")),
     path("books/", include("books.urls", namespace="book")),
+    path("borrowings/", include("borrowings.urls", namespace="borrowings")),
 ]
