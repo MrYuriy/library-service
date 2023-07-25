da33814ba1c5868899973137ea50eb6ad99ff185
YuriiBabiuk <118677914+YuriiBabiuk@users.noreply.github.com>
2023-07-25 19:26:34 +0300

    Add env varible and settings for stripe

--- payments/models.py
+++ payments/models.py
@@ -2,16 +2,23 @@ from django.db import models
 from enum import Enum
 from django.core.validators import URLValidator
 from borrowings.models import Borrowing
-from _decimal import Decimal
+from _decimal import Decimal, ROUND_HALF_EVEN
+import os
+import stripe
+from rest_framework.exceptions import ValidationError
 
 FINE_MULTIPLIER = 1.5
+
+
 class PaymentStatus(Enum):
     PENDING = "PENDING"
     PAID = "PAID"
 
+
 class PaymentType(Enum):
     PAYMENT = "PAYMENT"
     FINE = "FINE"
+
 
 class Payment(models.Model):
     status = models.CharField(
@@ -24,9 +31,7 @@ class Payment(models.Model):
         Borrowing, on_delete=models.CASCADE, related_name="payments"
     )
     stripe_sesion_url = models.TextField(
-        validators=[URLValidator()],
-        null=True,
-        blank=True
+        validators=[URLValidator()], null=True, blank=True
     )
     stripe_sesion_id = models.CharField(max_length=255, null=True, blank=True)
 
@@ -46,3 +51,59 @@ class Payment(models.Model):
             * self.borroving.book.dayly_fee
             * Decimal(FINE_MULTIPLIER)
         )
+
+    def create_stripe_session(self, success_url, cancel_url):
+        line_items = [
+            {
+                "price_data": {
+                    "currency": "usd",
+                    "product_data": {
+                        "name": f"{self.type} for {self.borrowing.book.title}",
+                    },
+                    "unit_amount": Decimal(
+                        self.money_to_pay.quantize(
+                            Decimal("0.01"), roundind=ROUND_HALF_EVEN
+                        )
+                    ),
+                },
+                "quantity": 1,
+            }
+        ]
+
+        stripe.api_key = os.getenv("STRIPE_SECRET_KEY")
+        session = stripe.checkout.Session.create(
+            payment_method_types=["card"],
+            line_items=line_items,
+            mode="payment",
+            success_url=success_url,
+            cancel_url=cancel_url,
+        )
+
+        self.stripe_session_id = session["id"]
+        self.stripe_session_url = session["url"]
+        self.save()
+
+        return session
+
+    def validate_unique(self, exclude=None):
+        super().validate_unique(exclude)
+
+        existing_payments = Payment.objects.filter(
+            borrowing=self.borrowing, type=self.type
+        )
+
+        if self.pk:
+            existing_payments = existing_payments.exclude(pk=self.pk)
+
+        if existing_payments.exists():
+            raise ValidationError(
+                {
+                    "type": f"A payment with type '{self.type}' already exists for this borrowing."
+                }
+            )
+    def save(self, *args, **kwargs):
+        self.validate_unique()
+        super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.status
