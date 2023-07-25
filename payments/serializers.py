bf79d0353c39806c795d05d669c4c4e48f881c80
YuriiBabiuk <118677914+YuriiBabiuk@users.noreply.github.com>
2023-07-25 19:11:43 +0300

    Add payment serializers.py

---
+++ payments/serializers.py
@@ -0,0 +1,17 @@
+from django.urls import reverse
+from .models import Payment
+from rest_framework import serializers
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = (
+            "id",
+            "status",
+            "type",
+            "borrowing",
+            "stripe_session_url",
+            "stripe_session_id",
+            "money_to_pay",
+        )
